6abd1ebeee321f2a30083864b26b23e5b953aaba
ahmad zakqi <zakqiahmad34@gmail.com>
2023-10-09 20:21:31 +0700

    pembuatan halaman depan dan about rentalbuku.net

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+public function index()
+{
+echo "<h1>Perkenalkan</h1>";
+echo"Nama saya Ahmad Zakqi
+Saya tingga di daerah Tanah abang
+olah raga yang saya sukai adalah
+Bulutangkis";}
+}
